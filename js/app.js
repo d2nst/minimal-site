4c3ae1cb509b60868e7d88d58e7d6b4a420540fe
d2nst <wlsdl950@gmail.com>
2023-06-29 20:22:35 +0900

    색 변경 제외 완료

--- js/app.js
+++ js/app.js
@@ -3,68 +3,68 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const headerText = document.querySelectorAll('.text');
-class SmoothScroll {
-  constructor({
-    element = window,
-    strength = 12,
-    acceleration = 1.2,
-    deceleration = 0.975,
-  } = {}) {
-    this.element = element;
-    this.distance = strength;
-    this.acceleration = acceleration;
-    this.deceleration = deceleration;
-    this.running = false;
+// class SmoothScroll {
+//   constructor({
+//     element = window,
+//     strength = 12,
+//     acceleration = 1.2,
+//     deceleration = 0.975,
+//   } = {}) {
+//     this.element = element;
+//     this.distance = strength;
+//     this.acceleration = acceleration;
+//     this.deceleration = deceleration;
+//     this.running = false;
 
-    this.element.addEventListener('wheel', this.scrollHandler.bind(this), {
-      passive: false,
-    });
-    this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {
-      passive: false,
-    });
-    this.scroll = this.scroll.bind(this);
-  }
+//     this.element.addEventListener('wheel', this.scrollHandler.bind(this), {
+//       passive: false,
+//     });
+//     this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {
+//       passive: false,
+//     });
+//     this.scroll = this.scroll.bind(this);
+//   }
 
-  scrollHandler(e) {
-    e.preventDefault();
+//   scrollHandler(e) {
+//     e.preventDefault();
 
-    if (!this.running) {
-      this.top = this.element.pageYOffset || this.element.scrollTop || 0;
-      this.running = true;
-      this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
-      this.isDistanceAsc = true;
-      this.scroll();
-    } else {
-      this.isDistanceAsc = false;
-      this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
-    }
-  }
+//     if (!this.running) {
+//       this.top = this.element.pageYOffset || this.element.scrollTop || 0;
+//       this.running = true;
+//       this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
+//       this.isDistanceAsc = true;
+//       this.scroll();
+//     } else {
+//       this.isDistanceAsc = false;
+//       this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+//     }
+//   }
 
-  scroll() {
-    if (this.running) {
-      this.currentDistance *=
-        this.isDistanceAsc === true ? this.acceleration : this.deceleration;
-      Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false
-        ? (this.running = false)
-        : 1;
-      Math.abs(this.currentDistance) >= Math.abs(this.distance)
-        ? (this.isDistanceAsc = false)
-        : 1;
+//   scroll() {
+//     if (this.running) {
+//       this.currentDistance *=
+//         this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+//       Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false
+//         ? (this.running = false)
+//         : 1;
+//       Math.abs(this.currentDistance) >= Math.abs(this.distance)
+//         ? (this.isDistanceAsc = false)
+//         : 1;
 
-      this.top += this.currentDistance;
-      this.element.scrollTo(0, this.top);
+//       this.top += this.currentDistance;
+//       this.element.scrollTo(0, this.top);
 
-      requestAnimationFrame(this.scroll);
-    }
-  }
-}
+//       requestAnimationFrame(this.scroll);
+//     }
+//   }
+// }
 
-const scroll = new SmoothScroll({
-  element: window,
-  strength: 12,
-  acceleration: 1.0,
-  deceleration: 0.975,
-});
+// const scroll = new SmoothScroll({
+//   element: window,
+//   strength: 12,
+//   acceleration: 1.0,
+//   deceleration: 0.975,
+// });
 
 let t1 = gsap.timeline();
 let t2 = gsap.timeline();
@@ -92,7 +92,6 @@ t2.fromTo(
 // 로고 위치, 사이즈 변경
 ScrollTrigger.create({
   animation: t1,
-  // markers: true,
   start: 'top 50%',
   end: '100px 30%',
   trigger: '.nav-logo',
@@ -100,7 +99,6 @@ ScrollTrigger.create({
 });
 ScrollTrigger.create({
   animation: t2,
-  // markers: true,
   start: 'top 50%',
   end: 'bottom 30%',
   trigger: '.nav-logo',
@@ -109,7 +107,6 @@ ScrollTrigger.create({
 
 // 네브바 상단고정
 ScrollTrigger.create({
-  // markers: true,
   start: 'top top',
   trigger: '.wrap',
   toggleClass: { targets: 'nav', className: 'active' },
@@ -121,7 +118,6 @@ headerText.forEach((item) => {
   let targetElement = item;
   let tl = gsap.timeline({
     scrollTrigger: {
-      // markers: true,
       trigger: triggerElement,
       start: '-100% top',
       end: '30% top',
@@ -141,7 +137,6 @@ let tl = gsap.timeline({
     start: 'top top',
     end: '+=1020 ',
     scrub: 1,
-    markers: true,
   },
 });
 tl.fromTo(
@@ -159,6 +154,156 @@ tl.fromTo(
     duration: 1,
   }
 );
-// Grid Title Change
-const titleList = document.querySelectorAll('.title-list');
-const imgList = document.querySelectorAll('.img-list');
+// Light to Dark Color Change
+const imgWrapOdd = document.querySelectorAll('.img__list:nth-child(odd)');
+
+imgWrapOdd.forEach((item) => {
+  let triggerElement = item;
+  let targetElement = ('body', '.container.o-nav');
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top 20%',
+      end: 'bottom bottom',
+      scrub: 1,
+      markers: true,
+    },
+  });
+  tl.fromTo(
+    targetElement,
+    {
+      backgroundColor: '#e8e2da',
+      color: '#131514',
+      duration: 1,
+    },
+    {
+      backgroundColor: '#131514',
+      color: '#e8e2da',
+      duration: 1,
+    }
+  );
+});
+
+// Dark to Light Color Change
+const contentWrap = document.querySelectorAll(
+  '.img__list:nth-child(even), .header-circle__wrap'
+);
+
+contentWrap.forEach((item) => {
+  let triggerElement = item;
+  let targetElement = ('body', '.container.o-nav');
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top top',
+      end: 'bottom bottom',
+      scrub: 1,
+      markers: true,
+    },
+  });
+  tl.fromTo(
+    targetElement,
+    {
+      backgroundColor: '#131514',
+      color: '#e8e2da',
+      duration: 1,
+    },
+    {
+      backgroundColor: '#e8e2da',
+      color: '#131514',
+      duration: 1,
+    }
+  );
+});
+
+// Title Change
+document.querySelector('.title-list').classList.add('o-title');
+const imgWrpper = document.querySelectorAll('.img__list');
+
+imgWrpper.forEach((triggerElement, index) => {
+  let myIndex = index;
+  let targetElement = document.querySelectorAll('.title-list')[myIndex];
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top bottom',
+      end: 'bottom bottom',
+      onEnter: () => {
+        document.querySelectorAll('.title-list').forEach((item) => {
+          item.classList.remove('o-title');
+        });
+        targetElement.classList.add('o-title');
+      },
+      onEnterBack: () => {
+        document.querySelectorAll('.title-list').forEach((item) => {
+          item.classList.remove('o-title');
+        });
+        targetElement.classList.add('o-title');
+      },
+    },
+  });
+});
+
+// Contents Image Move 1
+const firstImageMove = document.querySelectorAll('.listitem:nth-child(3n+1)');
+
+firstImageMove.forEach((item) => {
+  let triggerElement = item;
+  let targetElement = item;
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: 1,
+    },
+  });
+  tl.to(targetElement, {
+    y: '-30%',
+    duration: 1,
+  });
+});
+
+// Contents Image Move 3
+const secondImageMove = document.querySelectorAll('.listitem:nth-child(3n+2)');
+
+secondImageMove.forEach((item) => {
+  let triggerElement = item;
+  let targetElement = item;
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: 2,
+    },
+  });
+  tl.to(targetElement, {
+    y: '-50%',
+    duration: 1,
+  });
+});
+
+// Contents Image Move 3
+const thirdImageMove = document.querySelectorAll('.listitem:nth-child(3n+3)');
+
+thirdImageMove.forEach((item) => {
+  let triggerElement = item;
+  let targetElement = item;
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: 1.5,
+    },
+  });
+  tl.to(targetElement, {
+    y: '-30%',
+    duration: 1,
+  });
+});
