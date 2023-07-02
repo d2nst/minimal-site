a70e8344818cef8c4e960e75681351a128764df9
d2nst <wlsdl950@gmail.com>
2023-07-02 15:11:48 +0900

    div 추가

--- js/app.js
+++ js/app.js
@@ -3,73 +3,74 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const headerText = document.querySelectorAll('.text');
-class SmoothScroll {
-  constructor({
-    element = window,
-    strength = 12,
-    acceleration = 1.2,
-    deceleration = 0.975,
-  } = {}) {
-    this.element = element;
-    this.distance = strength;
-    this.acceleration = acceleration;
-    this.deceleration = deceleration;
-    this.running = false;
+// class SmoothScroll {
+//   constructor({
+//     element = window,
+//     strength = 12,
+//     acceleration = 1.2,
+//     deceleration = 0.975,
+//   } = {}) {
+//     this.element = element;
+//     this.distance = strength;
+//     this.acceleration = acceleration;
+//     this.deceleration = deceleration;
+//     this.running = false;
 
-    this.element.addEventListener('wheel', this.scrollHandler.bind(this), {
-      passive: false,
-    });
-    this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {
-      passive: false,
-    });
-    this.scroll = this.scroll.bind(this);
-  }
+//     this.element.addEventListener('wheel', this.scrollHandler.bind(this), {
+//       passive: false,
+//     });
+//     this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {
+//       passive: false,
+//     });
+//     this.scroll = this.scroll.bind(this);
+//   }
 
-  scrollHandler(e) {
-    e.preventDefault();
+//   scrollHandler(e) {
+//     e.preventDefault();
 
-    if (!this.running) {
-      this.top = this.element.pageYOffset || this.element.scrollTop || 0;
-      this.running = true;
-      this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
-      this.isDistanceAsc = true;
-      this.scroll();
-    } else {
-      this.isDistanceAsc = false;
-      this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
-    }
-  }
+//     if (!this.running) {
+//       this.top = this.element.pageYOffset || this.element.scrollTop || 0;
+//       this.running = true;
+//       this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
+//       this.isDistanceAsc = true;
+//       this.scroll();
+//     } else {
+//       this.isDistanceAsc = false;
+//       this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+//     }
+//   }
 
-  scroll() {
-    if (this.running) {
-      this.currentDistance *=
-        this.isDistanceAsc === true ? this.acceleration : this.deceleration;
-      Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false
-        ? (this.running = false)
-        : 1;
-      Math.abs(this.currentDistance) >= Math.abs(this.distance)
-        ? (this.isDistanceAsc = false)
-        : 1;
+//   scroll() {
+//     if (this.running) {
+//       this.currentDistance *=
+//         this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+//       Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false
+//         ? (this.running = false)
+//         : 1;
+//       Math.abs(this.currentDistance) >= Math.abs(this.distance)
+//         ? (this.isDistanceAsc = false)
+//         : 1;
 
-      this.top += this.currentDistance;
-      this.element.scrollTo(0, this.top);
+//       this.top += this.currentDistance;
+//       this.element.scrollTo(0, this.top);
 
-      requestAnimationFrame(this.scroll);
-    }
-  }
-}
+//       requestAnimationFrame(this.scroll);
+//     }
+//   }
+// }
 
-const scroll = new SmoothScroll({
-  element: window,
-  strength: 12,
-  acceleration: 1.0,
-  deceleration: 0.975,
-});
+// const scroll = new SmoothScroll({
+//   element: window,
+//   strength: 12,
+//   acceleration: 1.0,
+//   deceleration: 0.975,
+// });
 
+const introWidth = document.querySelector('.intro');
 let t1 = gsap.timeline();
 let t2 = gsap.timeline();
 t1.fromTo(
-  '.nav-logo',
+  ' .nav-logo',
   {
     y: -180,
   },
